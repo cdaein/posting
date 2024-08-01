93ca31eac0fb38067c2712647b0d6de88541ae0a
MBP14 <3522003+cdaein@users.noreply.github.com>
2024-08-01 08:56:59 -0700

    feat: bluesky image
    now it supports image(s) on bluesky.
    their `uploadBlob()` doc example was incomplete and inaccurate.

--- src/platforms/bluesky.ts
+++ src/platforms/bluesky.ts
@@ -1,6 +1,18 @@
-import { BskyAgent, PostRecord, RichText } from "@atproto/api";
+import { BlobRef, BskyAgent, PostRecord, RichText } from "@atproto/api";
 import { EnvVars, PostSettings } from "../types";
 import kleur from "kleur";
+import fs from "node:fs";
+import path from "node:path";
+import mime from "mime-types";
+
+type ImageRecord = {
+  alt: string;
+  image: BlobRef;
+  aspectRatio?: {
+    width: number;
+    height: number;
+  };
+};
 
 const { bold, green, yellow } = kleur;
 
@@ -14,10 +26,14 @@ export async function uploadBluesky(
     service: "https://bsky.social",
   });
 
-  await agent.login({
-    identifier: envVars.blueskyUsername,
-    password: envVars.blueskyPassword,
-  });
+  try {
+    await agent.login({
+      identifier: envVars.blueskyUsername,
+      password: envVars.blueskyPassword,
+    });
+  } catch (e) {
+    throw new Error(`Bluesky log in failed`);
+  }
 
   const { postType, bodyText, filenames } = settings;
 
@@ -28,20 +44,64 @@ export async function uploadBluesky(
   const rt = new RichText({ text: bodyText });
   await rt.detectFacets(agent); // automatically detects mentions and links
 
+  // https://docs.bsky.app/docs/tutorials/creating-a-post#images-embeds
+  const images: ImageRecord[] = [];
   for (const filename of filenames) {
-    //
+    const localFilePath = path.join(folderPath, filename);
+    const buffer = fs.readFileSync(localFilePath);
+    const mimeType = mime.lookup(localFilePath) as string;
+    try {
+      console.log(`Uploading file ${yellow(filename)}`);
+      const { data } = await agent.uploadBlob(buffer, {
+        encoding: mimeType,
+      });
+      images.push({
+        alt: "alt text",
+        image: data.blob,
+      });
+      console.log(`Uploaded file`);
+    } catch (e) {
+      console.error(e);
+      throw new Error(`Error uploading media to Bluesky \n${e}`);
+    }
   }
 
   const postRecord = {
     $type: "app.bsky.feed.post",
     text: rt.text,
     facets: rt.facets,
+    ...(filenames.length > 0
+      ? {
+          embed: {
+            $type: "app.bsky.embed.images",
+            images,
+          },
+        }
+      : {}),
     createdAt: new Date().toISOString(),
   };
 
-  console.log(`Publishing on ${bold("Bluesky")}..`);
-  const status = await agent.post(postRecord);
-  console.log(`Published on ${bold("Bluesky")}. uri: ${green(status.uri)}`);
+  try {
+    console.log(`Publishing on ${bold("Bluesky")}..`);
+    const status = await agent.post(postRecord);
+    console.log(`Published on ${bold("Bluesky")}. uri: ${green(status.uri)}`);
+    return status;
+  } catch (e) {
+    throw new Error(`Error publishing on Bluesky \n${e}`);
+  }
+}
 
-  return status;
+function convertDataURIToUint8Array(dataURI: string) {
+  // Strip off the data URI scheme and get the base64 string
+  const base64 = dataURI.split(",")[1];
+  // Decode the base64 string
+  // const binaryString = Buffer.from(base64, "base64").toString("binary");
+  const binaryString = atob(base64);
+
+  // Convert the binary string to a Uint8Array
+  const bytes = new Uint8Array(binaryString.length);
+  for (let i = 0; i < binaryString.length; i++) {
+    bytes[i] = binaryString.charCodeAt(i);
+  }
+  return bytes;
 }
