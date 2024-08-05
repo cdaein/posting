e5a2a95c5f08a261b48ab296892ffd0855c9b014
MBP14 <3522003+cdaein@users.noreply.github.com>
2024-08-04 21:16:11 -0700

    fix: twitter diff stats

--- src/platforms/twitter.ts
+++ src/platforms/twitter.ts
@@ -33,7 +33,31 @@ type MostRecentTweetStats = {
   };
 };
 
+export type TwitterStats = {
+  likes: number;
+  retweets: number;
+  replies: number;
+  quotes: number;
+  bookmarks: number;
+};
+
 const { bold, green, yellow } = kleur;
+
+const lastStats: Record<keyof TwitterStats, number | undefined> = {
+  likes: undefined,
+  retweets: undefined,
+  replies: undefined,
+  quotes: undefined,
+  bookmarks: undefined,
+};
+
+const diffStats: TwitterStats = {
+  likes: 0,
+  retweets: 0,
+  replies: 0,
+  quotes: 0,
+  bookmarks: 0,
+};
 
 export function initTwitterClient(envVars: EnvVars) {
   if (
@@ -133,31 +157,55 @@ export async function getTwitterStats(client: TwitterApiReadWrite) {
     });
 
     const username = user.data.username;
-    const { id, text, organic_metrics, non_public_metrics, public_metrics } =
-      user.includes?.tweets![0] as unknown as MostRecentTweetStats;
+    const { id, text, organic_metrics, public_metrics } = user.includes
+      ?.tweets![0] as unknown as MostRecentTweetStats;
 
-    // const impressions = `Impressions: ${green(public_metrics.impression_count)}`;
-    // const engagements = `Engagements: ${green(non_public_metrics.engagements)}`;
-    const likes = `Likes: ${green(organic_metrics.like_count)}`;
-    const retweets = `Retweets: ${green(organic_metrics.retweet_count)}`;
-    const reply = `Replies: ${green(organic_metrics.reply_count)}`;
-    const quotes = `Quotes: ${green(public_metrics.quote_count)}`;
-    const bookmarks = `Bookmarks: ${green(public_metrics.bookmark_count)}`;
+    // 1. get current stats
+    // 2. if no last stat (first run), set diff to cur stats
+    // 2. if last stat, get diff bewteen cur and last
+
+    const curStats: TwitterStats = {
+      likes: organic_metrics.like_count,
+      retweets: organic_metrics.retweet_count,
+      replies: organic_metrics.reply_count,
+      quotes: public_metrics.quote_count,
+      bookmarks: public_metrics.bookmark_count,
+    };
+
+    const keys = Object.keys(diffStats) as (keyof TwitterStats)[];
+    for (const key of keys) {
+      if (lastStats[key] === undefined) {
+        diffStats[key] = curStats[key];
+      } else {
+        diffStats[key] = curStats[key] - lastStats[key];
+      }
+    }
+
+    // if stat never changed (always 0), it doesn't display "+".
+    const getDiffStat = (prev: number | undefined, diff: number) => {
+      return (prev && diff >= 0 ? "+" : "") + diff.toString();
+    };
+
+    const { likes, retweets, replies, quotes, bookmarks } = diffStats;
+
+    // const impressionsStr = `Impressions: ${green(public_metrics.impression_count)}`;
+    // const engagementsStr = `Engagements: ${green(non_public_metrics.engagements)}`;
+    const likesStr = `Likes: ${green(getDiffStat(lastStats.likes, likes))}`;
+    const retweetsStr = `Retweets: ${green(getDiffStat(lastStats.retweets, retweets))}`;
+    const replyStr = `Replies: ${green(getDiffStat(lastStats.replies, replies))}`;
+    const quotesStr = `Quotes: ${green(getDiffStat(lastStats.quotes, quotes))}`;
+    const bookmarksStr = `Bookmarks: ${green(getDiffStat(lastStats.bookmarks, bookmarks))}`;
 
     // URL - https://x.com/[USERNAME]/status/[POST_ID]
     const postUrl = `https://x.com/${username}/status/${id}`;
-
     console.log(`Latest ${bold("Twitter")} (${green(postUrl)}) stats`);
     console.log(`Text: ${text}`);
-    console.log(
-      // impressions,
-      // engagements,
-      likes,
-      retweets,
-      reply,
-      quotes,
-      bookmarks,
-    );
+    console.log(likesStr, retweetsStr, replyStr, quotesStr, bookmarksStr);
+
+    // update last stat to current stat
+    for (const key of keys) {
+      lastStats[key] = curStats[key];
+    }
 
     // NOTE: reading user/tweet doesn't work in free API (only posting works..)
     // const user = await readOnlyClient.v2.userByUsername("cdaein");
